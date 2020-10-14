0b623c56a34aef34dd8284233dbff6c2c5bc3179
오주상 <dhwntkd412@sasa.hs.kr>
2020-10-14 17:25:27 +0900

    time clustring updated

--- cluster_time.py
+++ cluster_time.py
@@ -29,13 +29,33 @@ def date_str_to_score(date_str):
 
 # %%
 def cluster_with_date(news_list):
-    date_list = []
+    points = []
     for news in news_list:
-        date_list.append([date_str_to_score(news['published_at'])])
-    date_list = np.array(date_list)
+        points.append([date_str_to_score(news['published_at']),0])
+    points = np.array(points)
 
-    n_clusters = max(len(news_list) // 5, 2)
-    kmeans = KMeans(n_clusters=n_clusters).fit(date_list)
-    labels, centers = kmeans.labels_, kmeans.cluster_centers_
-    grouped = group_news_by_labels(news_list, labels)
-    return grouped
+    #k값을 1부터 k_max까지 돌려보고 최소의 WSS score를 탐색한다.
+    k_max = max(len(news_list)//2, 1)+1
+    
+    for k in range(1, k_max, 1):
+      kmeans = KMeans(n_clusters = k).fit(points)
+      centroids = kmeans.cluster_centers_
+      labels = kmeans.labels_
+      #wss 계산과정
+      pred_clusters = kmeans.predict(points)
+      curr_sse = 0
+      
+      # calculate square of Euclidean distance of each point from its cluster center and add to current WSS
+      for i in range(len(news_list)):
+        curr_center = centroids[pred_clusters[i]]
+        curr_sse += (points[i, 0] - curr_center[0]) ** 2 + (points[i, 1] - curr_center[1]) ** 2
+      #값 리스트에 저장  
+      #날짜가 같은 기사끼리 명확하게 군집화되면 이 점수가 0에 매우 급격히 수렴한다. 
+      #따라서  0.001 이하가 되는 최초의 k을 클러스터 수로 지정하고 label과 centroid를 return한다.
+      if curr_sse<0.0001:
+          grouped = group_news_by_labels(news_list, labels)
+          return grouped
+      #0.0001이하가 되지 않고 끝에 도달할 경우 그냥 마지막 label, centroid를 return
+      if k==k_max-1:
+          grouped = group_news_by_labels(news_list, labels)
+          return grouped
